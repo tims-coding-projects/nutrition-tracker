b111fb62893666e9445106e9bddedbf70f98154b
TimothyXZ <timscoding@protonmail.com>
2022-05-09 23:22:44 -0400

    23:22 5-9-22

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/******************************************************************************/
+
+  /*                    nutrition-tracker main script
+
+                          timscoding@protonmail.com
+
+                                                                              */
+
+// *****************************************************************************
+
+console.log('Testing JavaScript integration with HTML');
